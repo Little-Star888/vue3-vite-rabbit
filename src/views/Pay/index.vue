a232672b4a8f56d0fdd1ec8cba47bd5717e31465
Little-Star <1174415702@qq.com>
2023-05-20 16:05:28 +0800

    倒计时逻辑函数实现

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -2,13 +2,16 @@
 import { ref, onMounted } from "vue";
 import { getOrderDetailAPI } from "@/apis/pay";
 import { useRoute } from "vue-router";
+import { useCountDown } from "@/directives/useCountDown";
+const { formatTime, start } = useCountDown();
 const route = useRoute();
 const payInfo = ref({});
 const getPayInfo = async () => {
   try {
     const res = await getOrderDetailAPI(route.query.id);
     payInfo.value = res.data.result;
-    console.log(res);
+    // 初始化倒计时秒数
+    start(res.data.result.countdown);
   } catch (err) {
     console.log(err);
   }
@@ -33,7 +36,10 @@ const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redire
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>
+            支付还剩 <span>{{ formatTime }}</span
+            >, 超时后将取消订单
+          </p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
